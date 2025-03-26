37548eb2cdaade697d9720d5e22b8c7d571370c2
user5012 <kmamais100@gmail.com>
2025-03-26 23:27:52 +0200

    fixed bootstrap not loading correctly

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Games from "./pages/Games/Games";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
